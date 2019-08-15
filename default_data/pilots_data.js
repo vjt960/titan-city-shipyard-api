d13e5589960a5b5c90d4b8df0341f79eafbbaa05
Vinton Teo <taulago@ymail.com>
2019-08-14 19:36:31 -0600

    Create pilots dataset.

---
+++ default_data/pilots_data.js
@@ -0,0 +1,214 @@
+const pilotsData = [
+  {
+    serial: 2212588,
+    first: 'Andreea',
+    surname: 'Hanson',
+    callsign: 'Mimoza',
+    blacklist: false
+  },
+  {
+    serial: 4123675,
+    first: 'Anneke',
+    surname: 'McGrady',
+    callsign: 'Mom',
+    blacklist: false
+  },
+  {
+    serial: 1234567,
+    first: 'Emily',
+    surname: 'Dittmer',
+    callsign: 'GifyCat',
+    blacklist: false
+  },
+  {
+    serial: 7654321,
+    first: 'Hindreen',
+    surname: 'Abdullah',
+    callsign: 'Fawzi',
+    blacklist: false
+  },
+  {
+    serial: 0987656,
+    first: 'Jev',
+    surname: 'Forsberg',
+    callsign: 'Jazz',
+    blacklist: false
+  },
+  {
+    serial: 6787985,
+    first: 'Katie',
+    surname: 'Williams',
+    callsign: 'Cop',
+    blacklist: false
+  },
+  {
+    serial: 1122434,
+    first: 'Pat',
+    surname: 'Goulding',
+    callsign: 'Stickers',
+    blacklist: false
+  },
+  {
+    serial: 5545771,
+    first: 'Taylor',
+    surname: 'Jordan',
+    callsign: 'Fortune Teller',
+    blacklist: false
+  },
+  {
+    serial: 4402621,
+    first: 'Vinton',
+    surname: 'Teo',
+    callsign: 'Ganja',
+    blacklist: false
+  },
+  {
+    serial: 3324566,
+    first: 'Aidan',
+    surname: 'Kid',
+    callsign: 'Graffiti',
+    blacklist: false
+  },
+  {
+    serial: 8675309,
+    first: 'Ann',
+    surname: 'Cerveny',
+    callsign: 'SnapBack',
+    blacklist: false
+  },
+  {
+    serial: 1122334,
+    first: 'Anna',
+    surname: 'Olsen',
+    callsign: 'Who',
+    blacklist: false
+  },
+  {
+    serial: 2233114,
+    first: 'Antonio',
+    surname: 'Fry',
+    callsign: 'Cash Money',
+    blacklist: false
+  },
+  {
+    serial: 4454655,
+    first: 'Brandy',
+    surname: 'Mello',
+    callsign: 'On The Rox',
+    blacklist: false
+  },
+  {
+    serial: 9900822,
+    first: 'Brian',
+    surname: 'Byon',
+    callsign: 'B',
+    blacklist: false
+  },
+  {
+    serial: 7789543,
+    first: 'Colby',
+    surname: 'Allen',
+    callsign: 'DiCaprio',
+    blacklist: false
+  },
+  {
+    serial: 6758444,
+    first: 'David',
+    surname: 'Engel',
+    callsign: 'The Plug',
+    blacklist: false
+  },
+  {
+    serial: 8895443,
+    first: 'Emily',
+    surname: 'Lalonde',
+    callsign: 'The Other',
+    blacklist: false
+  },
+  {
+    serial: 6997243,
+    first: 'Evan',
+    surname: 'Markowitz',
+    callsign: 'Comrade',
+    blacklist: false
+  },
+  {
+    serial: 6998556,
+    first: 'Jacqueline',
+    surname: 'Moore',
+    callsign: 'Gardener',
+    blacklist: false
+  },
+  {
+    serial: 6334949,
+    first: 'Katherine',
+    surname: 'Lewis',
+    callsign: 'Ghost',
+    blacklist: false
+  },
+  {
+    serial: 8084545,
+    first: 'Kayla',
+    surname: 'Larson',
+    callsign: 'Wizard',
+    blacklist: false
+  },
+  {
+    serial: 3609199,
+    first: 'DeMarcus',
+    surname: 'Kirby',
+    callsign: 'TronKat',
+    blacklist: false
+  },
+  {
+    serial: 8910531,
+    first: 'Nathan',
+    surname: 'Froelich',
+    callsign: 'Cyborg',
+    blacklist: false
+  },
+  {
+    serial: 7200379,
+    first: 'Ryan',
+    surname: 'Flachman',
+    callsign: 'Marine',
+    blacklist: false
+  },
+  {
+    serial: 4756744,
+    first: 'Steve',
+    surname: 'Rumizen',
+    callsign: 'Maverick',
+    blacklist: false
+  },
+  {
+    serial: 9090888,
+    first: 'Kanye',
+    surname: 'West',
+    callsign: 'Genius',
+    blacklist: false
+  },
+  {
+    serial: 1202444,
+    first: 'Will',
+    surname: 'Mitchell',
+    callsign: 'wmitchell',
+    blacklist: false
+  },
+  {
+    serial: 8883456,
+    first: 'Pam',
+    surname: 'Lovett',
+    callsign: 'thatPamIam',
+    blacklist: false
+  },
+  {
+    serial: 9191991,
+    first: 'Kevin',
+    surname: 'Krato',
+    callsign: 'Crypto',
+    blacklist: false
+  }
+];
+
+module.exports = pilotsData;
