125481e39954d020041a355d3e8d44caeee23a0c
Vinton Teo <taulago@ymail.com>
2019-08-15 16:33:38 -0600

    Structure seed file to populate ships table with ships data.

--- seeds/ships.js
+++ seeds/ships.js
@@ -1,13 +1,24 @@
+const shipsData = require('../default_data/ships_data');
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+const createShip = (knex, ship) => {
+  return knex('ships').insert({
+    make: ship.make,
+    model: ship.model,
+    pad_size: ship.pad_size,
+    cost: ship.cost
+  });
+};
+
+exports.seed = knex => {
+  return knex('ships')
+    .del()
+    .then(() => {
+      return Promise.all(
+        shipsData.reduce((shipsPromises, ship) => {
+          shipsPromises.push(createShip(knex, ship));
+          return shipsPromises;
+        }, [])
+      );
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
 };
