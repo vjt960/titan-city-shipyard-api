1fe4770befad2a5d92c92f407e4df4e7eebb6619
Vinton Teo <taulago@ymail.com>
2019-08-14 19:36:55 -0600

    Create ships dataset.

---
+++ default_data/ships_data.js
@@ -0,0 +1,238 @@
+const shipsData = [
+  {
+    make: 'ZORGON PETERSON',
+    model: 'Adder',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Alliance Challenger',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Alliance Chieftan',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Alliance Crusader',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Anaconda',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Asp Explorer',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Asp Scout',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'SAUD KRUGER',
+    model: 'Beluga Liner',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Cobra Mk III',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Cobra Mk IV',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Diamondback',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Diamondback Scout',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'SAUD KRUGER',
+    model: 'Dophin',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'CORE DYNAMICS',
+    model: 'Eagle Mk II',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Explorer',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'CORE DYNAMICS',
+    model: 'Federal Assault Ship',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'CORE DYNAMICS',
+    model: 'Federal Corvette',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'CORE DYNAMICS',
+    model: 'Federal Dropship',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'CORE DYNAMICS',
+    model: 'Federal Gunship',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'ZORGON PETERSON',
+    model: 'Fer-de-Lance',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'ZORGON PETERSON',
+    model: 'Hauler',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'GUTAMAYA',
+    model: 'Imperial Clipper',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'GUTAMAYA',
+    model: 'Imperial Courier',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'GUTAMAYA',
+    model: 'Imperial Cutter',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'GUTAMAYA',
+    model: 'Imperial Eagle',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Keelback',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Krait MkII',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Krait Phantom',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'ZORGON PETERSON',
+    model: 'Mamba',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'SAUD KRUGER',
+    model: 'Orca',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Python',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Sidewinder Mk I',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Type-10 Defender',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Type-6 Transporter',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Type-7 Transporter',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'LAKON SPACEWAYS',
+    model: 'Type-9 Heavy',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Viper Mk III',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'FAULCON DELACY',
+    model: 'Viper Mk IV',
+    pad_size: 'size',
+    cost: 9999
+  },
+  {
+    make: 'CORE DYNAMICS',
+    model: 'Vulture',
+    pad_size: 'size',
+    cost: 9999
+  }
+];
+
+module.exports = shipsData;
