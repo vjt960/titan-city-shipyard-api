2ec1f46859ae88329d37df6f565d39056eaecb0a
Vinton Teo <taulago@ymail.com>
2019-08-18 16:27:53 -0600

    Change connection to localhost.

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: 'postgres://localhost/shipyard',
     useNullAsDefault: true,
     migrations: {
       directory: './migrations'
